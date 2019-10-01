106343b364529f6c725d651c935ee38b2676a884
Iwan Pletnew <osmiwol@gmail.com>
2019-10-01 10:06:35 +0500

    Добавил тесты

--- ConsoleNotes/NotesTests/UnitTest1.cs
+++ ConsoleNotes/NotesTests/UnitTest1.cs
@@ -8,12 +8,45 @@ namespace NotesTests
     {
         [Theory]
         [InlineData("header", "description", 0)]
+        [InlineData(null, null, 0)]
+        [InlineData(null, null, 100)]
         public void CreateNoteTest(string header, string description, int id)
         {
             Note note = new Note(header, description);
 
             NoteController noteController = new NoteController();
             noteController.Create(header, description);
+
+            Assert.Equal(note, noteController.GetNote(id));
+        }
+
+
+        [Theory]
+        [InlineData("header", "description", 0)]
+        [InlineData(null, null, 0)]
+        [InlineData(null, null, 100)]
+        public void DeleteNoteTest(string header, string description, int id)
+        {
+            Note note = new Note(header, description);
+
+            NoteController noteController = new NoteController();
+            noteController.Create(header, description);
+            noteController.Delete(id);
+
+            Assert.NotEqual(note, noteController.GetNote(id));
+        }
+
+        [Theory]
+        [InlineData("header", "description", 0)]
+        [InlineData(null, null, 0)]
+        [InlineData(null, null, 100)]
+        public void UpdateNoteTest(string header, string description, int id)
+        {
+            Note note = new Note("васильки", "цветочки");
+
+            NoteController noteController = new NoteController();
+            noteController.Create(header, description);
+            noteController.Update(id, "васильки", "цветочки");
 
             Assert.Equal(note, noteController.GetNote(id));
         }
